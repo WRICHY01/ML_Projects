b211821e877fd13752ab536fa3b2ea3e05f73d1a
WRICHY01 <101552972+WRICHY01@users.noreply.github.com>
2025-05-14 16:44:36 +0100

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -86,14 +86,6 @@ class DataCleaningStrategy(DataPreprocessingStrategy):
         logging.error(f"Error in handling data: {e}")
         raise e
 
-
-if __name__ == "__main__":
-    data_df = pd.read_csv(r"\my_projects\customer_satisfaction_project\dataset\olist_customers_dataset.csv")
-    data_clean_test = DataPreprocessStrategy().handle_data(data_df)
-    print(data_clean_test.columns)
-    data_split_test = DataSplitStrategy().handle_data(data_clean_test)
-    print(data_split_test)
-
     
 
 
