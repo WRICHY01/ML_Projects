85c59e8f91e931c0785a43c9a55848f5618322f2
WRICHY01 <101552972+WRICHY01@users.noreply.github.com>
2025-05-14 16:44:58 +0100

    Delete src/tempCodeRunnerFile.py

--- src/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    data_df = pd.read_csv(r"\my_projects\customer_satisfaction_project\dataset\olist_customers_dataset.csv")
-    data_clean_test = DataPreprocessStrategy().handle_data(data_df)
-    print(data_clean_test.columns)
-    data_split_test = DataSplitStrategy().handle_data(data_clean_test)
-    pri
